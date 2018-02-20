da0d28f0d972f67204900764a7ba16852def29ee
Raul <runnerdude08@gmail.com>
2018-02-20 11:58:55 -0800

    Added source comp, tweaked home & app html/css, fixed footer to stay low

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SourceComponent } from './source/source.component';
 
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
+    { path: 'sources', component: SourceComponent},
     { path: '**', redirectTo: 'home' }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SourceComponent } from './source/source.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SourceComponent
   ],
   imports: [
     BrowserModule,
