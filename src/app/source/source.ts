e0c2518c617deb7db7d524967717edbdb29d1ddc
Raul <runnerdude08@gmail.com>
2018-02-23 15:50:48 -0800

    Added articles property to Source.ts

--- src/app/source/source.ts
+++ src/app/source/source.ts
@@ -1,7 +1,10 @@
+import { Article } from "../article/article";
+
 export class Source {
     constructor(
         public id?: number,
         public name?: string,
-        public website?: string
+        public website?: string,
+        public articles?: Article[]
     ){}
 }
