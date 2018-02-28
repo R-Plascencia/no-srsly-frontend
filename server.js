088e5763516b893e910043f9078fcbfefe0d5dec
Raul <runnerdude08@gmail.com>
2018-02-28 14:22:49 -0800

    Changed title in index, modifying route in server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('*', function(req, res) {
-    res.sendfile('./src/index.html')
+    res.sendfile(__dirname + '/dist/index.html')
 })
