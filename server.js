a7a1501b012af33a9e41435b15a05e72aaadf2bd
Raul <runnerdude08@gmail.com>
2018-02-28 13:53:09 -0800

    Refer any routes to the index file

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+app.get('*', function(req, res) {
+    res.sendfile('./src/index.html')
+})
